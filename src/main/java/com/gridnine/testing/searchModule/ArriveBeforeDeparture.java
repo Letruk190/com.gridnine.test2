91e774ea2031ab53c05fd7b1d5657d768166e940
Letruk190 <Letruk190@yandex.ru>
2022-01-15 16:40:25 +0300

    new1

--- src/main/java/com/gridnine/testing/searchModule/ArriveBeforeDeparture.java
+++ src/main/java/com/gridnine/testing/searchModule/ArriveBeforeDeparture.java
@@ -3,6 +3,7 @@ package com.gridnine.testing.searchModule;
 import com.gridnine.testing.testClasses.Flight;
 
 
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,8 +13,17 @@ public class ArriveBeforeDeparture implements FlightFilter {
     @Override
     public List<Flight> getFilteredFlights(List<Flight> notFiltered) {
         List<Flight> filtered = notFiltered.stream().
-                filter(f -> f.getSegments().get(0).getDepartureDate().isBefore(f.getSegments().get(0).getArrivalDate()))
-                .collect(Collectors.toList());
+                filter(f -> {
+                    long time = 0;
+                    for (int i = 0; i < f.getSegments().size() - 1; i++) {
+                        if (f.getSegments().get(i).getArrivalDate().isAfter(f.getSegments().get(i+1).getDepartureDate())) {
+                            time = 0;
+                        } else {
+                            time = 1;
+                        }
+                    }
+                    return time > 0;
+                }).collect(Collectors.toList());
         return filtered;
     }
 }
--- src/main/java/com/gridnine/testing/searchModule/DepartureBeforeNow.java
+++ src/main/java/com/gridnine/testing/searchModule/DepartureBeforeNow.java
@@ -9,18 +9,10 @@ import java.util.stream.Collectors;
 public class DepartureBeforeNow implements FlightFilter {
     @Override
     public List<Flight> getFilteredFlights(List<Flight> notFiltered) {
-        List<Flight> filtered = notFiltered.stream().
-                filter(f -> {
-                    long time = 0;
-                    for (int i = 0; i < f.getSegments().size() - 1; i++) {
-                        if (f.getSegments().get(i).getArrivalDate().isAfter(f.getSegments().get(i+1).getDepartureDate())) {
-                            time = 0;
-                        } else {
-                            time = 1;
-                        }
-                    }
-                    return time > 0;
-                }).collect(Collectors.toList());
+        List<Flight> filtered = notFiltered.stream()
+                .filter(f->f.getSegments().get(0).getDepartureDate().isBefore(LocalDateTime.now()))
+                .collect(Collectors.toList());
         return filtered;
     }
 }
+
